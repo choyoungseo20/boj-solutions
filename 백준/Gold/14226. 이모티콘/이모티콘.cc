27f12ac284cbd437c5f9560cec191d74420666ff
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2025-08-17 23:15:53 +0900

    [Gold IV] Title: 이모티콘, Time: 4 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+bool visited[2001][2001];
+
+int main() {
+    int S;
+    cin >> S;
+
+    queue<pair<pair<int, int>, int>> q;
+    visited[1][0] = true;
+    q.push({ {1, 0}, 0 });
+
+    while (!q.empty()) {
+        int emoticon_num = q.front().first.first;
+        int clipboard_num = q.front().first.second;
+        int time = q.front().second;
+        q.pop();
+
+        if (emoticon_num == S) {
+            cout << time;
+            break;
+        }
+        
+        if (!visited[emoticon_num][emoticon_num]) {
+            visited[emoticon_num][emoticon_num] = true;
+            q.push({ {emoticon_num, emoticon_num}, time + 1 });
+        }
+        if (emoticon_num + clipboard_num <= 2000 && clipboard_num != 0 && !visited[emoticon_num + clipboard_num][clipboard_num]) {
+            visited[emoticon_num + clipboard_num][clipboard_num] = true;
+            q.push({ {emoticon_num + clipboard_num, clipboard_num}, time + 1 });
+        }
+        if (emoticon_num - 1 > 0 && !visited[emoticon_num - 1][clipboard_num]) {
+            visited[emoticon_num - 1][clipboard_num] = true;
+            q.push({ {emoticon_num - 1, clipboard_num}, time + 1 });
+        }
+    }
+}
