b0af38bb6a534b24df45096b9bd50a6f5c56fabd
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2025-09-20 17:33:06 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    string s;
+    for (int i = 0; i < T; i++) {
+        cin >> s;
+        
+        cout << s[0] << s[s.size() - 1] << "\n";
+    }
+}
