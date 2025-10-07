81a6f244b0eb219aade2cc6113178ef4c903b5c9
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2025-10-08 04:51:30 +0900

    [Silver I] Title: Generic Queries, Time: 708 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/16713. Generic Queries/Generic Queries.cc
+++ 백준/Silver/16713. Generic Queries/Generic Queries.cc
@@ -2,26 +2,28 @@
 
 using namespace std;
 
-int A[1000001] = {0};
+int A[1000001];
 
 int main() {
+    // 빠른 입출력
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
     int N, Q;
     cin >> N >> Q;
     
-    int num;
+    int a;
     for (int i = 1; i <= N; i++) {
-        cin >> num;
-        A[i] = num ^ A[i - 1];
+        cin >> a;
+        A[i] = a ^ A[i - 1]; // XOR 연산자 ^
     }
     
     int ans = 0;
     int s, e;
+    // XOR을 두 번 하면 원래대로 돌아오는 특성을 이용하여 누적합처럼 사용
     while (Q--) {
         cin >> s >> e;
-        ans ^= A[e] ^ A[s - 1];
+        ans ^= A[e] ^ A[s - 1]; // XOR 연산자 ^
     }
     
     cout << ans;
