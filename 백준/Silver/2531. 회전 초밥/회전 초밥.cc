23bae5770ec720b7c405d856ee8b3240800b0463
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2025-10-13 21:17:51 +0900

    [Silver I] Title: 회전 초밥, Time: 8 ms, Memory: 2256 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N, d, k, c;
+	cin >> N >> d >> k >> c;
+	
+	vector<int> v(2 * N);
+	vector<int> cnt(d + 1, 0);
+
+	cnt[c]++;
+	int eat_cnt = 1;
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+		v[N + i] = v[i];
+
+		if (i < k) {
+			if (cnt[v[i]] == 0) {
+				eat_cnt++;
+			}
+			cnt[v[i]]++;
+		}
+	}
+
+	int ans = eat_cnt;
+	int i = 0;
+	while (1) {
+		if (i >= N) {
+			break;
+		}
+
+		cnt[v[i]]--;
+		if (cnt[v[i]] == 0) eat_cnt--;
+
+		if (cnt[v[i + k]] == 0) {
+			eat_cnt++;
+			ans = max(ans, eat_cnt);
+		}
+		cnt[v[i + k]]++;
+		
+		i++;
+	}
+
+	cout << ans;
+}
