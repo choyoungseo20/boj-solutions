6c6ea965d3bbeabf906a1f5fdc6abc4e2fa3dfcb
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-18 02:34:14 +0900

    [Platinum V] Title: 버블 소트, Time: 408 ms, Memory: 12052 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long ans;
+
+vector<int> merge_sort(vector<int> v) {
+	if (v.size() == 1) return v;
+
+	int mid = v.size() / 2;
+	vector<int> l = merge_sort(vector<int>(v.begin(), v.begin() + mid));
+	vector<int> r = merge_sort(vector<int>(v.begin() + mid, v.end()));
+
+	int i = 0, j = 0;
+	vector<int> res;
+	while (i < l.size() && j < r.size()) {
+		if (l[i] > r[j]) {
+			res.push_back(r[j]);
+			ans += l.size() - i;
+			j++;
+		}
+		else {
+			res.push_back(l[i]);
+			i++;
+		}
+	}
+	while (i < l.size()) {
+		res.push_back(l[i]);
+		i++;
+	}
+	while (j < r.size()) {
+		res.push_back(r[j]);
+		j++;
+	}
+
+	return res;
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	vector<int> solve = merge_sort(v);
+
+	cout << ans;
+}
