79f8a3fd840c9ebdad5e8478f9642720e37d0316
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-20 16:58:04 +0900

    [Platinum III] Title: 달리기, Time: 60 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.cc
+++ 백준/Platinum/16930. 달리기/달리기.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 
 using namespace std;
 
@@ -14,15 +13,17 @@ int d[1001][1001];
 int ans = -1;
 
 void bfs(int max_move) {
-	queue<pair<int, pair<int, int>>> q;
-	q.push({ 0, {x_1, y_1} });
+	vector<pair<int, pair<int, int>>> q;
+	q.reserve(1000000);
+	q.push_back({ 0, {x_1, y_1} });
 	d[x_1][y_1] = 0;
+	int head = 0;
 
-	while (!q.empty()) {
-		int dist = q.front().first;
-		int x = q.front().second.first;
-		int y = q.front().second.second;
-		q.pop();
+	while (head < q.size()) {
+		int dist = q[head].first;
+		int x = q[head].second.first;
+		int y = q[head].second.second;
+		head++;
 
 		for (int i = 0; i < 4; i++) {
 			for (int j = 1; j <= max_move; j++) {
@@ -35,7 +36,7 @@ void bfs(int max_move) {
 								ans = dist + 1;
 								return;
 							}
-							q.push({ dist + 1, {nx, ny} });
+							q.push_back({ dist + 1, {nx, ny} });
 							d[nx][ny] = dist + 1;
 						}
 					}
