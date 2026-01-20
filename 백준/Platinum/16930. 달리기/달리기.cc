0645730dae9c3557ecaccc9bfd63ee93d57380d0
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-20 16:53:42 +0900

    [Platinum III] Title: 달리기, Time: 368 ms, Memory: 7168 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.cc
+++ 백준/Platinum/16930. 달리기/달리기.cc
@@ -10,42 +10,10 @@ int dy[4] = { 0, 1, 0, -1 };
 int n, m, k;
 int x_1, y_1, x_2, y_2;
 char map[1001][1001];
-bool visited[1001][1001];
 int d[1001][1001];
 int ans = -1;
 
-int bfs1() {
-	queue<pair<int, pair<int, int>>> q;
-	q.push({ 0, {x_1, y_1} });
-	visited[x_1][y_1] = true;
-
-	int res = -1;
-	while (!q.empty()) {
-		int dist = q.front().first;
-		int x = q.front().second.first;
-		int y = q.front().second.second;
-		q.pop();
-
-		if (x == x_2 && y == y_2) {
-			res = dist;
-			break;
-		}
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx > 0 && nx <= n && ny > 0 && ny <= m) {
-				if (map[nx][ny] == '.' && !visited[nx][ny]) {
-					q.push({ dist + 1, {nx, ny} });
-					visited[nx][ny] = true;
-				}
-			}
-		}
-	}
-	return res;
-}
-
-
-void bfs() {
+void bfs(int max_move) {
 	queue<pair<int, pair<int, int>>> q;
 	q.push({ 0, {x_1, y_1} });
 	d[x_1][y_1] = 0;
@@ -58,7 +26,7 @@ void bfs() {
 
 		vector<pair<int, pair<int, int>>> v[4];
 		for (int i = 0; i < 4; i++) {
-			for (int j = 1; j <= k; j++) {
+			for (int j = 1; j <= max_move; j++) {
 				int nx = x + j * dx[i];
 				int ny = y + j * dy[i];
 				if (nx > 0 && nx <= n && ny > 0 && ny <= m) {
@@ -68,7 +36,7 @@ void bfs() {
 								ans = dist + 1;
 								return;
 							}
-							v[i].push_back({dist + 1, {nx, ny}});
+							v[i].push_back({ dist + 1, {nx, ny} });
 							d[nx][ny] = dist + 1;
 						}
 					}
@@ -76,7 +44,7 @@ void bfs() {
 				}
 			}
 		}
-		for (int j = k - 1; j >= 0; j--) {
+		for (int j = max_move - 1; j >= 0; j--) {
 			for (int i = 0; i < 4; i++) {
 				if (j < v[i].size()) {
 					q.push(v[i][j]);
@@ -98,16 +66,21 @@ int main() {
 			d[i][j] = 987654321;
 		}
 	}
-	
+
 	cin >> x_1 >> y_1 >> x_2 >> y_2;
 
-	int res = bfs1();
-	if (res == -1 || k == 1) {
-		cout << res;
+	bfs(1);
+	if (ans == -1 || k == 1) {
+		cout << ans;
 		return 0;
 	}
 
-	bfs();
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			d[i][j] = 987654321;
+		}
+	}
+	bfs(k);
 
 	cout << ans;
 }
