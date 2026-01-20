e6fa778e12e73da9d8e7af55e2e3d684e57f41e7
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-20 17:16:43 +0900

    [Platinum III] Title: 달리기, Time: 32 ms, Memory: 18632 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.cc
+++ 백준/Platinum/16930. 달리기/달리기.cc
@@ -1,56 +1,20 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 
-int n, m, k;
-int x_1, y_1, x_2, y_2;
 char map[1001][1001];
 int d[1001][1001];
-int ans = -1;
-
-void bfs(int max_move) {
-	queue<pair<int, pair<int, int>>> q;
-	q.push({ 0, {x_1, y_1} });
-	d[x_1][y_1] = 0;
-
-	while (!q.empty()) {
-		int dist = q.front().first;
-		int x = q.front().second.first;
-		int y = q.front().second.second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			for (int j = 1; j <= max_move; j++) {
-				int nx = x + j * dx[i];
-				int ny = y + j * dy[i];
-				if (nx > 0 && nx <= n && ny > 0 && ny <= m) {
-					if (map[nx][ny] == '.') {
-						if (d[nx][ny] > dist + 1) {
-							if (nx == x_2 && ny == y_2) {
-								ans = dist + 1;
-								return;
-							}
-							q.push({ dist + 1, {nx, ny} });
-							d[nx][ny] = dist + 1;
-						}
-						else if (d[nx][ny] < dist + 1) break;
-					}
-					else break;
-				}
-				else break;
-			}
-		}
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	int n, m, k;
+	int x_1, y_1, x_2, y_2;
 	cin >> n >> m >> k;
 
 	for (int i = 1; i <= n; i++) {
@@ -62,20 +26,39 @@ int main() {
 
 	cin >> x_1 >> y_1 >> x_2 >> y_2;
 
-	/*
-	bfs(1);
-	if (ans == -1 || k == 1) {
-		cout << ans;
-		return 0;
-	}
+	int ans = -1;
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			d[i][j] = 987654321;
+	vector<pair<int, pair<int, int>>> q;
+	q.reserve(1000000);
+	q.push_back({ 0, {x_1, y_1} });
+	d[x_1][y_1] = 0;
+	int head = 0;
+
+	while (head < q.size()) {
+		int dist = q[head].first;
+		int x = q[head].second.first;
+		int y = q[head].second.second;
+		head++;
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 1; j <= k; j++) {
+				int nx = x + j * dx[i];
+				int ny = y + j * dy[i];
+				if (nx <= 0 || nx > n || ny <= 0 || ny > m) break;
+				if (map[nx][ny] != '.') break;
+				if (d[nx][ny] < dist + 1) break;
+				if (d[nx][ny] > dist + 1) {
+					if (nx == x_2 && ny == y_2) {
+						ans = dist + 1;
+						cout << ans;
+						return 0;
+					}
+					q.push_back({ dist + 1, {nx, ny} });
+					d[nx][ny] = dist + 1;
+				}
+			}
 		}
 	}
-	*/
-	bfs(k);
 
 	cout << ans;
 }
