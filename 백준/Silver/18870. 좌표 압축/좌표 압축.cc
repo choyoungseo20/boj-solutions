9300163ddc3dc14cb282c5b2bf7ab39a754ff3d1
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-21 12:35:06 +0900

    [Silver II] Title: 좌표 압축, Time: 360 ms, Memory: 20584 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
-#include <unordered_map>
 
 using namespace std;
+
+struct Info {
+	int x;
+	int idx;
+	int cnt;
+};
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -13,20 +17,40 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> v(n);
-	set<int> s;
+	vector<Info> v;
+	int x;
 	for (int i = 0; i < n; i++) {
-		cin >> v[i];
-		s.insert(v[i]);
+		cin >> x;
+		v.push_back({ x, i, 0 });
 	}
 
-	unordered_map<int, int> hash_map;
-	int i = 0;
-	for (auto cnt : s) {
-		hash_map[cnt] = i++;
+	sort(v.begin(), v.end(),
+		[](const Info& a, const Info& b) {
+			return a.x < b.x;
+		});
+
+
+	int before_x;
+	int dup = 0;
+	for (int i = 0; i < n; i++) {
+		if (i == 0) {
+			before_x = v[i].x;
+		}
+		else {
+			if (before_x == v[i].x) dup++;
+			
+			v[i].cnt = i - dup;
+			before_x = v[i].x;
+		}
 	}
 
+
+	sort(v.begin(), v.end(),
+		[](const Info& a, const Info& b) {
+			return a.idx < b.idx;
+		});
+
 	for (int i = 0; i < n; i++) {
-		cout << hash_map[v[i]] << " ";
+		cout << v[i].cnt << " ";
 	}
 }
