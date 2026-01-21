701215b55523802089affc3e6b11a60cef64d0e7
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-21 19:39:55 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
-int dist[501][501];
 int tri[501][501];
-
-int n;
-int ans;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
+	int n;
 	cin >> n;
 
 	for (int i = 1; i <= n; i++) {
@@ -28,32 +24,4 @@ int main() {
 	}
 	
 	cout << tri[1][1];
-	/*
-	queue < pair<int, pair<int, int>>> q;
-	dist[1][1] = 7;
-	q.push({ tri[1][1], {1, 1} });
-
-	while (!q.empty()) {
-		int d = q.front().first;
-		int x = q.front().second.first;
-		int y = q.front().second.second;
-		q.pop();
-
-		if (x >= n) {
-			ans = max(ans, d);
-			continue;
-		}
-
-		if (dist[x + 1][y] < d + tri[x + 1][y]) {
-			dist[x + 1][y] = d + tri[x + 1][y];
-			q.push({ dist[x + 1][y], {x + 1, y} });
-		}
-		if (dist[x + 1][y + 1] < d + tri[x + 1][y + 1]) {
-			dist[x + 1][y + 1] = d + tri[x + 1][y + 1];
-			q.push({ dist[x + 1][y + 1], {x + 1, y + 1} });
-		}
-	}
-
-	cout << ans;
-	*/
 }
