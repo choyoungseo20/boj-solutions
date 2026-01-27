bead97f5503eecc90e253091ad9d65a2db7bde4f
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-27 16:23:43 +0900

    [Gold IV] Title: 문자열 폭발, Time: 40 ms, Memory: 10812 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -5,7 +5,6 @@
 using namespace std;
 
 int bomb_idx_cnt[37]; // 폭발 문자열 인덱스별 필요 개수
-int last_idx[62];
 
 vector<pair<int, int>> next_info[63];
 
@@ -13,6 +12,12 @@ int bomb_char_cnt[62];
 int char_cnt[62];
 
 bool visited[1000000];
+
+int ctoi(char c) {
+	if (c >= '0' && c <= '9') return c - '0';
+	if (c >= 'a' && c <= 'z') return c - 'a' + 10;
+	if (c >= 'A' && c <= 'Z') return c - 'A' + 10 + 26;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -25,11 +30,7 @@ int main() {
 
 	int s_c, l_c;
 	for (int i = 0; i < bomb_size; i++) {
-		int c;
-
-		if (bomb[i] >= '0' && bomb[i] <= '9') c = bomb[i] - '0';
-		if (bomb[i] >= 'a' && bomb[i] <= 'z') c = bomb[i] - 'a' + 10;
-		if (bomb[i] >= 'A' && bomb[i] <= 'Z') c = bomb[i] - 'A' + 10 + 26;
+		int c = ctoi(bomb[i]);
 
 		if (i == 0) {
 			s_c = c;
@@ -41,7 +42,6 @@ int main() {
 		}
 
 		bomb_char_cnt[c]++;
-
 		bomb_idx_cnt[i] = bomb_char_cnt[c];
 	}
 
@@ -50,11 +50,7 @@ int main() {
 	int c_c = 62;
 	vector<int> tmp_bomb_idx;
 	for (int i = 0; i < str_size; i++) {
-		int str_c, bomb_c;
-
-		if (str[i] >= '0' && str[i] <= '9') str_c = str[i] - '0';
-		if (str[i] >= 'a' && str[i] <= 'z') str_c = str[i] - 'a' + 10;
-		if (str[i] >= 'A' && str[i] <= 'Z') str_c = str[i] - 'A' + 10 + 26;
+		int str_c = ctoi(str[i]);
 
 		if (!is_bomb) {
 			if (str[i] == bomb[0]) {
@@ -71,33 +67,23 @@ int main() {
 			}
 		}
 		else {
-			tmp_bomb_idx.push_back(i);
-
 			int k = -1;
-			// 잘 생각해보고 고치자
-			// abac
-			// abc
-			// a -> 틀림
 			if (str_c == s_c) {
 				k = 0;
 			}
 			for (int j = 0; j < next_info[c_c].size(); j++) {
 				auto ni = next_info[c_c][j];
 				if (str_c == ni.first) {
-
-					if (bomb[ni.second - 1] >= '0' && bomb[ni.second - 1] <= '9') bomb_c = bomb[ni.second - 1] - '0';
-					if (bomb[ni.second - 1] >= 'a' && bomb[ni.second - 1] <= 'z') bomb_c = bomb[ni.second - 1] - 'a' + 10;
-					if (bomb[ni.second - 1] >= 'A' && bomb[ni.second - 1] <= 'Z') bomb_c = bomb[ni.second - 1] - 'A' + 10 + 26;
+					int bomb_c = ctoi(bomb[ni.second - 1]);
 
 					if (char_cnt[bomb_c] >= bomb_idx_cnt[ni.second - 1]) {
 						k = ni.second;
 					}
-					else {
-						break;
-					}
+					else break;
 				}
-				
 			}
+
+			tmp_bomb_idx.push_back(i);
 			if (k != -1) {
 				char_cnt[str_c]++;
 				c_c = str_c;
@@ -114,10 +100,7 @@ int main() {
 				if (tmp_bomb_idx.size() != 0) {
 					char tmp = str[tmp_bomb_idx[tmp_bomb_idx.size() - 1]];
 
-					if (tmp >= '0' && tmp <= '9') c_c = tmp - '0';
-					if (tmp >= 'a' && tmp <= 'z') c_c = tmp - 'a' + 10;
-					if (tmp >= 'A' && tmp <= 'Z') c_c = tmp - 'A' + 10 + 26;
-
+					c_c = ctoi(tmp);
 				}
 				else {
 					is_bomb = false;
