5ba54ee4e891586ded17e404acba34efd8b76fc7
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-27 17:21:23 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int mul[5][5][40];
+int ans[5][5];
+int tmp[5][5];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	long long b;
+	cin >> n >> b;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> mul[i][j][1];
+			if (i == j) ans[i][j] = 1;
+		}
+	}
+
+	int idx = 1;
+	long long p = 1;
+	while (b >= p) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					mul[i][j][idx + 1] += mul[i][k][idx] * mul[k][j][idx];
+					mul[i][j][idx + 1] %= 1000;
+				}
+			}
+		}
+		p *= 2;
+		idx++;
+	}
+
+	while (b) {
+		while (b < p) {
+			p /= 2;
+			idx--;
+		}
+
+		b -= p;
+
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					tmp[i][j] += ans[i][k] * mul[k][j][idx];
+					tmp[i][j] %= 1000;
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				ans[i][j] = tmp[i][j];
+				tmp[i][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << ans[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
