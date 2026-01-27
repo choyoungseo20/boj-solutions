4325363184d244f0ee3a31ecd3cef3545e86611d
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-01-27 17:40:10 +0900

    [Gold IV] Title: 플로이드, Time: 88 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dist[101][101];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (i == j) dist[i][j] = 0;
+			else dist[i][j] = 987654321;
+		}
+	}
+
+	int u, v, c;
+	for (int i = 0; i < m; i++) {
+		cin >> u >> v >> c;
+
+		dist[u][v] = min(dist[u][v], c);
+	}
+
+	for (int x = 1; x <= n; x++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i == j) continue;
+
+				for (int k = 1; k <= n; k++) {
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (dist[i][j] == 987654321) dist[i][j] = 0;
+			cout << dist[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
