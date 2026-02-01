1c99bb6709d0b27c2ec9e13d4138314990d1fe36
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-02-01 16:35:45 +0900

    [Gold IV] Title: 스도쿠, Time: 252 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.cc
+++ 백준/Gold/2239. 스도쿠/스도쿠.cc
@@ -4,61 +4,56 @@
 using namespace std;
 
 char map[9][9];
+bool visited[10][9][3];
 
 bool check_sq(int i, int j, int k) {
 	int i_m = i / 3;
 	int j_m = j / 3;
 
-	for (int x = 0 + 3 * i_m; x < 3 + 3 * i_m; x++) {
-		for (int y = 0 + 3 * j_m; y < 3 + 3 * j_m; y++) {
-			if (map[x][y] - '0' == k) {
-				return false;
-			}
-		}
-	}
-
-	return true;
+	return !visited[k][i_m * 3 + j_m][0];
 }
 
 bool check_row(int j, int k) {
-	for (int i = 0; i < 9; i++) {
-		if (map[i][j] - '0' == k) {
-			return false;
-		}
-	}
 
-	return true;
+	return !visited[k][j][1];
 }
 
 bool check_column(int i, int k) {
-	for (int j = 0; j < 9; j++) {
-		if (map[i][j] - '0' == k) {
-			return false;
-		}
-	}
 
-	return true;
+	return !visited[k][i][2];
 }
 
 bool check(int i, int j, int k) {
 	return check_sq(i, j, k) && check_row(j, k) && check_column(i, k);
 }
 
-bool rec(int x, int y) {
-	for (int i = x; i < 9; i++) {
-		for (int j = y; j < 9; j++) {
+bool rec() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
 			if (map[i][j] == '0') {
 				bool possible = false;
 				for (int k = 1; k <= 9; k++) {
 					if (check(i, j, k)) {
 						map[i][j] = k + '0';
-						possible = rec(0, 0);
+
+						int i_m = i / 3;
+						int j_m = j / 3;
+						visited[map[i][j] - '0'][i_m * 3 + j_m][0] = true;
+						visited[map[i][j] - '0'][j][1] = true;
+						visited[map[i][j] - '0'][i][2] = true;
+
+						possible = rec();
 
 						if (possible) return true;
+						else {
+							visited[map[i][j] - '0'][i_m * 3 + j_m][0] = false;
+							visited[map[i][j] - '0'][j][1] = false;
+							visited[map[i][j] - '0'][i][2] = false;
+							map[i][j] = '0';
+						}
 					}
 				}
 				if (!possible) {
-					map[i][j] = '0';
 					return false;
 				}
 			}
@@ -75,10 +70,16 @@ int main() {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			cin >> map[i][j];
+
+			int i_m = i / 3;
+			int j_m = j / 3;
+			visited[map[i][j] - '0'][i_m * 3 + j_m][0] = true;
+			visited[map[i][j] - '0'][j][1] = true;
+			visited[map[i][j] - '0'][i][2] = true;
 		}
 	}
 
-	if (rec(0, 0)) {
+	if (rec()) {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				cout << map[i][j];
