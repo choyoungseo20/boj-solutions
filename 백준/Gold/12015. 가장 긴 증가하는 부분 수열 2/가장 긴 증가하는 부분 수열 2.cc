aa2fc1d37232caf328047f22d9edfdfb02071168
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-02-03 14:15:06 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 160 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int ans;
+int a[1000001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+		a[i + 1] = 1234567890;
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		int l = 0;
+		int r = ans;
+
+		while (l <= r) {
+			int mid = (l + r) / 2;
+
+			if (v[i] > a[mid]) {
+				if (a[mid + 1] > v[i]) a[mid + 1] = v[i];
+				ans = max(ans, mid + 1);
+				l = mid + 1;
+			}
+			else {
+				r = mid - 1;
+			}
+		}
+	}
+
+	cout << ans;
+}
