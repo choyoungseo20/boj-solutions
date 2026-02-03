4e59871dc185e84533ef1891a5a21013b16eeb59
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-02-03 14:31:13 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 216 ms, Memory: 26348 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 1234567890;
+
+int ans;
+int a[1000001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	vector<pair<int, int>> log;
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+		a[i + 1] = INF;
+	}
+
+	a[0] = -INF;
+	for (int i = 0; i < v.size(); i++) {
+		int l = 0;
+		int r = ans;
+
+		while (l <= r) {
+			int mid = (l + r) / 2;
+
+			if (v[i] > a[mid]) {
+				if (a[mid + 1] > v[i]) {
+					log.push_back({ mid + 1, v[i] });
+					a[mid + 1] = v[i];
+				}
+				ans = max(ans, mid + 1);
+				l = mid + 1;
+			}
+			else {
+				r = mid - 1;
+			}
+		}
+	}
+
+	vector<int> la;
+	int idx = ans;
+	for (int i = log.size() - 1; i >= 0; i--) {
+		if (idx == 0) break;
+		if (idx != log[i].first) continue;
+		la.push_back(log[i].second);
+		idx--;
+	}
+
+	reverse(la.begin(), la.end());
+
+	cout << ans << "\n";
+	for (int i = 0; i < ans; i++) {
+		cout << la[i] << " ";
+	}
+}
