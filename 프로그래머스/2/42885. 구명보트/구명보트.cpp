40374b4c14d5a0890eaad92381cbcd7ca934d69e
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2026-05-13 10:20:06 +0900

    [level 2] Title: 구명보트, Time: 1.41 ms, Memory: 4.54 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(), people.end());
+    
+    int i = 0, j = people.size() - 1;
+    while (i < j) {
+        if (people[i] + people[j] <= limit) {
+            answer++;
+            i++;
+            j--;
+        }
+        else {
+            j--;
+        }
+    }
+    
+    answer += people.size() - (2 * answer);
+    
+    return answer;
+}
